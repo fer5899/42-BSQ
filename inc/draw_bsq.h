1b8115a462c0a57ac367320c9db3548201246dd1
Fernando Gomez De La Varga <fgomez-d@c1r9s3.42malaga.com>
2022-11-07 19:42:11 +0100

    Finish and test draw_bsq

--- inc/draw_bsq.h
+++ inc/draw_bsq.h
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   structs.h                                          :+:      :+:    :+:   */
+/*   draw_bsq.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/07 18:06:53 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/11/07 19:06:27 by fgomez-d         ###   ########.fr       */
+/*   Created: 2022/11/07 18:59:05 by fgomez-d          #+#    #+#             */
+/*   Updated: 2022/11/07 19:23:06 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUCTS_H
-# define STRUCTS_H
+#ifndef DRAW_BSQ_H
+# define DRAW_BSQ_H
 
-typedef struct s_obs {
-	int	row;
-	int	col;
-}	t_obs;
+# include "../inc/structs.h"
+# include "../inc/print_map.h"
 
-typedef struct s_square {
-	int	sp_row;
-	int	sp_col;
-	int	ep_row;
-	int	ep_col;
-}	t_square;
+char	**draw_bsq(char **map, int n_rows, t_square bsq);
 
 #endif
--- src/draw_bsq.c
+++ src/draw_bsq.c
@@ -1,28 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   structs.h                                          :+:      :+:    :+:   */
+/*   draw_bsq.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/07 18:06:53 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/11/07 19:06:27 by fgomez-d         ###   ########.fr       */
+/*   Created: 2022/11/07 18:59:05 by fgomez-d          #+#    #+#             */
+/*   Updated: 2022/11/07 19:39:49 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUCTS_H
-# define STRUCTS_H
+#include "../inc/draw_bsq.h"
 
-typedef struct s_obs {
+char	**draw_bsq(char **map, int n_rows, t_square bsq)
+{
 	int	row;
 	int	col;
-}	t_obs;
 
-typedef struct s_square {
-	int	sp_row;
-	int	sp_col;
-	int	ep_row;
-	int	ep_col;
-}	t_square;
-
-#endif
+	row = 1;
+	col = 0;
+	while (row < n_rows)
+	{
+		while (col < str_len(map[1]))
+		{
+			if ((bsq.sp_row <= row && row <= bsq.ep_row) \
+				&& (bsq.sp_col <= col && col <= bsq.ep_col))
+			{
+				map[row][col] = map[0][str_len(map[0]) - 2];
+			}
+			col++;
+		}
+		col = 0;
+		row++;
+	}
+	return (map);
+}
