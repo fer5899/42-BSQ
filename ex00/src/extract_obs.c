24cc4625ed655ffa7d6f61f7ae5e629151ac8b24
Fernando Gomez De La Varga <fgomez-d@c2r9s4.42malaga.com>
2022-11-08 18:07:02 +0100

    Fix compilation problems

--- ex00/src/extract_obs.c
+++ ex00/src/extract_obs.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:08:50 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/11/08 16:32:20 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/11/08 18:04:22 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,61 +54,64 @@ t_point	*extract_obstacles(char **map, int n_rows)
 		while (col < str_len(map[1]))
 		{
 			if (map[row][col] == map[0][str_len(map[0]) - 3])
-				init_pt(obs_arr++, row, col);
+			{
+				*obs_arr = init_pt(row, col);
+				obs_arr++;
+			}
 			col++;
 		}
 		col = 0;
 		row++;
 	}
-	init_pt(obs_arr, -1, -1);
+	*obs_arr = init_pt(-1, -1);
 	return (aux);
 }
 
 
 // TESTING
 
-int	main(void)
-{
-	// t_point	*obs_arr;
-	int		n_rows;
-	char	**map;
-	// int		obs_idx;
-	// int		n_obs;
-	t_square	bsq;
-	t_point		sp;
+// int	main(void)
+// {
+// 	// t_point	*obs_arr;
+// 	int		n_rows;
+// 	char	**map;
+// 	// int		obs_idx;
+// 	// int		n_obs;
+// 	t_square	bsq;
+// 	t_point		sp;
 
-	init_pt(&sp, 3, 3);
-	n_rows = 0;
-	// obs_idx = 0;
-	// n_obs = 0;
-	// bsq.sp.row = 4;
-	// bsq.sp.col = 0;
-	// bsq.ep.row = 5;
-	// bsq.ep.col = 4;
+// 	init_pt(&sp, 3, 1);
+// 	n_rows = 0;
+// 	// obs_idx = 0;
+// 	// n_obs = 0;
+// 	// bsq.sp.row = 4;
+// 	// bsq.sp.col = 0;
+// 	// bsq.ep.row = 5;
+// 	// bsq.ep.col = 4;
 	
-	map = get_map("map1.map", &n_rows);
-	print_map(map, n_rows);
-	bsq = get_start_sq(n_rows, 5, sp);
-	printf("sp_row %d, sp_col: %d, ep_row: %d, ep_col: %d\n", bsq.sp.row, \
-			bsq.sp.col, bsq.ep.row, bsq.ep.col);
-	insert_bsq(map, n_rows, bsq);
-	print_map(map, n_rows);
-	// printf("%d\n", n_rows);
-	// print_map(map, n_rows);
-	// obs_arr = extract_obstacles(map, n_rows);
-	// map = insert_bsq(map, n_rows, bsq);
-	// write(1, "\n", 1);
-	// print_map(map, n_rows);
-	// printf("\nSquare is valid: %d\n\n", sq_is_valid(bsq, obs_arr));
-	//printf("%d\n", count_obstacles(map, n_rows));
-	// while (obs_idx < n_obs)
-	// {
-	// 	printf("obstacle %d, row: %d, col: %d\n", obs_idx, \
-	// 		obs_arr[obs_idx].row, obs_arr[obs_idx].col);
-	// 	obs_idx++;
-	// }
-	//init_sq(&bsq, 0, 0);
-	//printf("sp_row %d, sp_col: %d, ep_row: %d, ep_col: %d\n", bsq.sp.row,
-	//		bsq.sp.col, bsq.ep.row, bsq.ep.col);
-}
+// 	map = get_map("map1.map", &n_rows);
+// 	print_map(map, n_rows);
+// 	bsq = get_start_sq(n_rows, 5, sp);
+// 	printf("sp_row %d, sp_col: %d, ep_row: %d, ep_col: %d\n", bsq.sp.row, \
+// 			bsq.sp.col, bsq.ep.row, bsq.ep.col);
+// 	insert_bsq(map, n_rows, bsq);
+// 	print_map(map, n_rows);
+// 	// printf("%d\n", n_rows);
+// 	// print_map(map, n_rows);
+// 	// obs_arr = extract_obstacles(map, n_rows);
+// 	// map = insert_bsq(map, n_rows, bsq);
+// 	// write(1, "\n", 1);
+// 	// print_map(map, n_rows);
+// 	// printf("\nSquare is valid: %d\n\n", sq_is_valid(bsq, obs_arr));
+// 	//printf("%d\n", count_obstacles(map, n_rows));
+// 	// while (obs_idx < n_obs)
+// 	// {
+// 	// 	printf("obstacle %d, row: %d, col: %d\n", obs_idx, \
+// 	// 		obs_arr[obs_idx].row, obs_arr[obs_idx].col);
+// 	// 	obs_idx++;
+// 	// }
+// 	//init_sq(&bsq, 0, 0);
+// 	//printf("sp_row %d, sp_col: %d, ep_row: %d, ep_col: %d\n", bsq.sp.row,
+// 	//		bsq.sp.col, bsq.ep.row, bsq.ep.col);
+// }
 
--- ex00/src/find_bsq.c
+++ ex00/src/find_bsq.c
@@ -6,13 +6,11 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 21:51:35 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/11/08 16:59:24 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/11/08 18:05:57 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/headers.h"
-
-#include <stdio.h>
 
 t_square	get_start_sq(int n_rows, int n_cols, t_point sp)
 {
@@ -33,27 +31,54 @@ t_square	get_start_sq(int n_rows, int n_cols, t_point sp)
 	return (start_sq);
 }
 
-// t_square	find_bsq(char **map, int n_rows, t_point *obs_arr)
-// {
-// 	t_square	bsq;
-// 	t_square	next_bsq;
-// 	t_point		sp;
-// 	int			n_cols;
+int	sq_size(t_square sq)
+{
+	return (sq.ep.row - sq.ep.col);
+}
 
-// 	n_cols = str_len(map[1]);
-// 	init_pt(&sp, n_rows, n_cols);
-// 	init_sq(&bsq, n_rows, n_cols);
-// 	while (sp.row > 1)
-// 	{
-// 		while (sp.col > 0)
-// 		{
-// 			if (map[sp.row][sp.col] != map[0][str_len(map[0]) - 3])
-// 				// Find biggest square from this row col and store in next_bsq
-// 				bsq = compare_squares(bsq, next_bsq);
-// 			sp.col--;
-// 		}
-// 		sp.col = n_cols;
-// 		sp.row--;
-// 	}
-// 	// Si bsq final es bsq caso base y hay obstaculo en ese sitio, devuelve bsq con -1
-// }
+t_square	rec_find_bsq(t_square sq, t_square minsq, t_point *obs_arr)
+{
+	if (sq_size(sq) < sq_size(minsq))
+		return (init_sq(init_pt(0, 0), init_pt(-1, -1)));
+	if (!sq_is_valid(sq, obs_arr))
+	{
+		sq.ep.row--;
+		sq.ep.col--;
+		return (rec_find_bsq(sq, minsq, obs_arr));
+	}
+	else
+		return (sq);
+}
+
+t_square	find_bsq(char **map, int n_rows, t_point *obs_arr)
+{
+	t_square	bsq;
+	t_square	next_bsq;
+	t_square	start_sq;
+	t_point		sp;
+	int			n_cols;
+
+	n_cols = str_len(map[1]);
+	sp = init_pt(n_rows - 1, n_cols - 1);
+	bsq = init_sq(sp, sp);
+	while (sp.row >= 1)
+	{
+		while (sp.col >= 0)
+		{
+			if (map[sp.row][sp.col] != map[0][str_len(map[0]) - 3])
+			{
+				start_sq = get_start_sq(n_rows, n_cols, sp);
+				next_bsq = rec_find_bsq(start_sq, bsq, obs_arr);
+				bsq = compare_squares(bsq, next_bsq);
+			}
+			sp.col--;
+		}
+		sp.col = n_cols;
+		sp.row--;
+	}
+	if ((bsq.sp.row == (n_rows - 1)) && (bsq.sp.col == (n_cols - 1)) && \
+		(map[n_rows - 1][n_cols - 1] == map[0][str_len(map[0]) - 3]))
+		return (init_sq(init_pt(0, 0), init_pt(-1, -1)));
+	return (bsq);
+	// Si bsq final es bsq caso base y hay obstaculo en ese sitio, devuelve bsq con -1
+}
