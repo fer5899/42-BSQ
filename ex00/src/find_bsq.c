d0fdd69fdb6d3c775d38c45736b0223b51aef17e
Fernando Gomez De La Varga <fgomez-d@c2r9s4.42malaga.com>
2022-11-08 20:37:33 +0100

    Finish alpha version

--- ex00/src/find_bsq.c
+++ ex00/src/find_bsq.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 21:51:35 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/11/08 19:48:34 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/11/08 20:27:56 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,10 +56,8 @@ t_square	find_bsq(char **map, int n_rows, t_point *obs_arr)
 	t_square	next_bsq;
 	t_square	start_sq;
 	t_point		sp;
-	int			n_cols;
 
-	n_cols = str_len(map[1]) - 1;
-	sp = init_pt(n_rows - 1, n_cols - 1);
+	sp = init_pt(n_rows - 1, str_len(map[1]) - 1 - 1);
 	bsq = init_sq(sp, sp);
 	while (sp.row >= 1)
 	{
@@ -67,18 +65,16 @@ t_square	find_bsq(char **map, int n_rows, t_point *obs_arr)
 		{
 			if (map[sp.row][sp.col] != map[0][str_len(map[0]) - 3])
 			{
-				start_sq = get_start_sq(n_rows, n_cols, sp);
+				start_sq = get_start_sq(n_rows, str_len(map[1]) - 1, sp);
 				next_bsq = rec_find_bsq(start_sq, bsq, obs_arr);
 				bsq = compare_squares(bsq, next_bsq);
 			}
 			sp.col--;
 		}
-		sp.col = n_cols - 1;
+		sp.col = str_len(map[1]) - 2;
 		sp.row--;
 	}
-	if ((bsq.sp.row == (n_rows - 1)) && (bsq.sp.col == (n_cols - 1)) && \
-		(map[n_rows - 1][n_cols - 1] == map[0][str_len(map[0]) - 3]))
+	if (count_obstacles(map, n_rows) == ((n_rows - 1) * (str_len(map[1]) - 1)))
 		return (init_sq(init_pt(0, 0), init_pt(-1, -1)));
 	return (bsq);
-	// Si bsq final es bsq caso base y hay obstaculo en ese sitio, devuelve bsq con -1
 }
