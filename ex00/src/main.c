4eec627e35cc344ccaa6c9a153ae4f6917f6f86e
Fernando Gomez De La Varga <fgomez-d@c2r9s4.42malaga.com>
2022-11-09 10:26:26 +0100

    Extract map iteration from main into own function

--- ex00/src/main.c
+++ ex00/src/main.c
@@ -6,41 +6,48 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:34:02 by bramos-l          #+#    #+#             */
-/*   Updated: 2022/11/08 19:49:21 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/11/09 10:24:56 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/headers.h"
 
-int	main(int argc, char **argv)
+#include <stdio.h>
+
+void	iterate_args(int argc, char **argv)
 {
-	int			aux;
-	int			n_rows;
 	char		**map;
 	t_square	bsq;
+	int			n_rows;
+	int			arg_idx;
 
-	bsq = init_sq(init_pt(0, 0), init_pt(-1, -1));
-	aux = 0;
+	arg_idx = 1;
+	while (arg_idx < argc)
+	{
+		n_rows = 0;
+		map = get_map(argv[arg_idx], &n_rows);
+		if (check_map(map, n_rows) == 0)
+			write(1, "map error\n", 11);
+		else
+		{
+			bsq = p_find_bsq(map, n_rows, extract_obstacles(map, n_rows));
+			if (bsq.ep.row != -1)
+			{
+				insert_bsq(map, n_rows, bsq);
+			}
+			print_map(map, n_rows);
+		}
+		arg_idx++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
 	if (argc == 1)
-		stdin_bsq();
+		return (0);// stdin_bsq();
 	else
 	{
-		aux = 1;
-		while (aux < argc)
-		{
-			n_rows = 0;
-			map = get_map(argv[aux], &n_rows);
-			if (check_map(map, n_rows) == 0)
-				write(1, "map error\n", 11);
-			else
-			{
-				bsq = find_bsq(map, n_rows, extract_obstacles(map, n_rows));
-				if (bsq.ep.row != -1)
-					insert_bsq(map, n_rows, bsq);
-				print_map(map, n_rows);
-			}
-			aux++;
-		}
+		iterate_args(argc, argv);
 	}
 	return (0);
 }
